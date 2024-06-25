d2acef5211d675c0d7cdb70c29f25b21d261941a
Norman Anderson <147473940+anormananderson@users.noreply.github.com>
2024-06-25 00:37:28 +0000

    feat: initially set up meilisearch index for products table

--- apps/web/clients/meilisearch.ts
+++ apps/web/clients/meilisearch.ts
@@ -1,10 +1,46 @@
 import { env } from "env.mjs"
-import { MeiliSearch } from "meilisearch"
+import { MeiliSearch, Meilisearch } from "meilisearch"
 
 const meilisearchClientSingleton = () => {
-  return new MeiliSearch({
+  const client = new MeiliSearch({
     host: env.MEILISEARCH_HOST || "",
     apiKey: env.MEILISEARCH_MASTER_KEY || "",
+  })
+  meilisearchSetup(client)
+  return client
+}
+
+const meilisearchSetup = async (client: MeiliSearch) => {
+  console.log("Beginning to set up Meilisearch product's table...")
+
+  client.getIndex('products').then((index) => {
+    console.log(`Index 'products' already exists with uid ${index.uid}`)
+  }).catch(async () => {
+    const demoData = await import('@enterprise-commerce/web/public/demo-data.json')
+    const products = demoData['results']
+    // Automatically create and populate 'products' index
+    client.index('products').addDocuments(products)
+      .then((res) => console.log(res))
+
+    const settings = {
+      "filterableAttributes": ["collections", "collections.title", "flatOptions", "handle", "minPrice", "tags", "variants.availableForSale", "vendor"],
+      "sortableAttributes": ["minPrice", "updatedAtTimestamp"],
+      "rankingRules":
+        [
+          "sort",
+          "words",
+          "typo",
+          "proximity",
+          "attribute",
+          "exactness"
+        ]
+    }
+  
+    client.index('products').updateSettings(settings)
+      .then((res) => console.log(res))
+    
+  }).then(() => {
+    console.log("Finished setting up Meilisearch product's table.")
   })
 }
 
